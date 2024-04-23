9e354cdef3d4c4a461332f18faa116a07163efdc
Thiago Bonfim <thiago.bonfim.ext@synchro.com.br>
2024-04-23 20:00:38 -0300

    Ajust de comentario

--- src/main/java/com/smartlock/View/util/PrintEntities.java
+++ src/main/java/com/smartlock/View/util/PrintEntities.java
@@ -8,24 +8,7 @@ import java.util.UUID;
  * printEntities
  */
 public class PrintEntities {
-    /*
-     * 
-     * public void printClass(Object object) {
-     * 
-     * Class<?> entity = object.getClass();
-     * 
-     * Field[] attributes = entity.getDeclaredFields();
-     * 
-     * for (Field attibute : attributes) {
-     * attibute.setAccessible(true);
-     * try {
-     * System.out.println(attibute.getName() + ": " + attibute.get(object));
-     * } catch (IllegalAccessException e) {
-     * e.printStackTrace();
-     * }
-     * }
-     * }
-     */
+
     public void printClass(Object object) {
         printClass(object, 0);
     }
